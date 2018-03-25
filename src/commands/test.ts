bf0922e3f24845f6e526ca84a8c774c7254be7e7
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-03-25 19:14:05 +1100

    Fix test child process

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,25 +1,47 @@
 import * as path from 'path';
-const {exec} = require('child_process');
+import {exec, spawn} from 'child_process';
 import {binPath} from '../utils';
-// const jestCli = require('jest-cli');
-//   jestCli.runCLI(config, [__dirname], () => {
-
-//   });
 
 export const test = async () => {
   const jestBin = binPath('jest');
   const configPath = path.resolve(__dirname, '../../jest.config.js');
-  const command = `${jestBin} -c ${configPath}`;
-  console.log('test', command);
-  // await exec();
-
+  // const command = `${jestBin} -c ${configPath}`;
+  
   try {
-    const subprocess = exec(command, {
-      env: {...process.env, FORCE_COLOR: true}
+    // TODO: default to --watch but spread other params if present
+    const subprocess = spawn(jestBin, ['-c', configPath, '--watch'], {
+      env: {...process.env, FORCE_COLOR: true},
+      stdio: 'inherit'
     });
 
-    subprocess.stdout.pipe(process.stdout);
-    subprocess.stderr.pipe(process.stdout);
+    // subprocess.stdin.pipe(process.stdin);
+    // subprocess.stdout.pipe(process.stdout);
+    // subprocess.stderr.pipe(process.stdout);
+
+
+    // process.stdout.on('data', (data) => {
+    //   console.log('subprocess stdout data', data)
+    // });
+
+    // process.stdin.on('data', (data) => {
+    //   console.log('subprocess stdin data', data)
+    // });
+
+    // process.stdout.on('message', (message) => {
+    //   console.log('subprocess stdout message', message)
+    // });
+
+    // process.stdin.on('message', (message) => {
+    //   console.log('subprocess stdin message', message)
+    // });
+
+    // process.on('message', (message) => {
+    //   console.log('message', message)
+    // });
+
+    // process.on('data', (data) => {
+    //   console.log('data', data)
+    // });
 
     subprocess.on('exit', (code: number) => process.exit(code));
   } catch (e) {
