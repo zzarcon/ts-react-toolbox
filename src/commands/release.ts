bf9f0c7c3b0533c00e4cc8f918dc378561208016
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-04-01 21:54:28 +1000

    wip

--- src/commands/release.ts
+++ src/commands/release.ts
@@ -1,10 +1,11 @@
 import { green, exec } from "../utils";
 
 export const release = async () => {
-  green('Creating new version tag 💥');
-  await exec('npm version patch');
-  green('Pushing tag ⛏');
-  await exec('git push --tags && git push');
+  // green('Creating new version tag 💥');
+  // await exec('npm version patch');
+  // green('Pushing tag ⛏');
+  // await exec('git push --tags && git push');
   green('Publishing to the registry 📦');
-  await exec('npm publish');
+  // await exec('npm publish');
+  await exec('yarn publish --silent --new-version patch');
 }
