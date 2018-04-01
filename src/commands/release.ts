500a4f9d518c12aa8dfd7fe8304fa70172ddd7de
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-04-01 22:20:42 +1000

    wip

--- src/commands/release.ts
+++ src/commands/release.ts
@@ -1,22 +1,24 @@
 import { green, exec } from "../utils";
 import { spawn } from "child_process";
 
+const publish = () => (
+  new Promise(resolve => {
+    const log = (data: string) => console.log(`${data}`);
+    const child = spawn('yarn', ['publish', '--silent', '--new-version', 'patch'], {
+      env: {...process.env, FORCE_COLOR: true}
+    });
+  
+    child.stdout.on('data', log);
+    child.stderr.on('data', log);
+    child.on('close', resolve);
+  })
+);
+
 export const release = async () => {
   // TODO: run "prepublishOnly": "yarn test:ci && yarn build"
 
   green('Publishing to the registry 📦');
-  
-  const child = spawn('yarn', ['publish', '--silent', '--new-version', 'patch'], {
-    env: {...process.env, FORCE_COLOR: true}
-  });
-
-  child.stdout.on('data', (data) => {
-    console.log(`${data}`);
-  });
-  
-  child.stderr.on('data', (data) => {
-    console.log(`${data}`);
-  });
+  await publish();
 
   green('Pushing tag ⛏');
   await exec('git push --tags && git push');
