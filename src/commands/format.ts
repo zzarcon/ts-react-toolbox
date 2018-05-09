9bd07cf608fb8bc7463d49d0f9bdcc5757220430
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-05-09 20:14:08 +1000

    wip

--- src/commands/format.ts
+++ src/commands/format.ts
@@ -1,15 +1,19 @@
-// import * as path from 'path';
+import * as path from 'path';
 import { green, spawn, binPath } from '../utils';
 
 export const format = async () => {
   const prettier = binPath('prettier-eslint');
-  // const configPath = path.resolve(__dirname, '../../configs/tslint.json');
+  const configPath = path.resolve(__dirname, '../../configs/prettier.config.js');
 
   green('Running Prettier 🥑');
   
+  // TODO: pass config path or options
+
   await spawn(prettier, [
     'src/**/*.+(ts|tsx)',
-    '--write'
+    '--write',
+    '--config',
+    configPath
   ]);
 
   // node_modules/.bin/prettier --no-editorconfig --write
