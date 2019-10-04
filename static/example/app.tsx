013d7f06914f492aa0bebd177a29fe04d34c496a
Hector Leon Zarco Garcia <hezarco@gmail.com>
2019-10-04 19:10:01 +1000

    Remove class usage in example App

--- static/example/app.tsx
+++ static/example/app.tsx
@@ -1,25 +1,17 @@
 import * as React from 'react';
-import {Component} from 'react';
 import {GHCorner} from 'react-gh-corner';
 import {AppWrapper, GlobalStyles} from './styled';
 
-export interface AppState {
-  
-}
-
 const repoUrl = 'https://github.com/';
-export default class App extends Component <{}, AppState> {
-  state: AppState = {
-    
-  }
 
-  render() {
-    return (
-      <AppWrapper>
-        <GlobalStyles />
-        <GHCorner openInNewTab href={repoUrl} />
-        Example!
-      </AppWrapper>
-    )
-  }
+const App = () => {
+  return (
+    <AppWrapper>
+      <GlobalStyles />
+      <GHCorner openInNewTab href={repoUrl} />
+      Example!
+    </AppWrapper>
+  )
 }
+
+export default App
