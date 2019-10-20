7e8bc3f3f19ddcaf78450acb30da7ac216aa7ebc
Hector Leon Zarco Garcia <hezarco@gmail.com>
2019-10-20 14:58:42 +1100

    Convert component into a function

--- static/src/component.tsx
+++ static/src/component.tsx
@@ -1,18 +1,11 @@
 import * as React from 'react';
-import {Component} from 'react';
 
 export interface ComponentProps {
   
 }
 
-export interface ComponentState {
-  
-}
-
-export class ComponentName extends Component<ComponentProps, ComponentState> {
-  render() {
-    return (
-      <div></div>
-    );
-  }
+export const ComponentName = (props: ComponentProps) => {
+  return (
+    <div></div>
+  );
 }
